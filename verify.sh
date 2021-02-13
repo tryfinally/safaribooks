4c10faa890df700caefa0240b18fd9ec2cc29ebe
Muhammad Zahalqa <m@tryfinally.com>
2021-02-13 23:38:23 +0200

    verify script to get book downloads that did not result in epub created

---
+++ verify.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $# != 1 ]]; then
+	echo "Usage: ${0} download directory, like Books/"
+	exit 1
+fi
+
+echo "Verifying $1 ..."
+echo "Book that did not download"
+for d in ${1}/*/ ; do
+	flag=0
+	book=""
+    for f in "${d}"* ; do
+    	if [[ $f == *.epub ]]; then
+    		flag=1
+    		book=$f
+    	fi
+    done
+    if [[ $flag == 0 ]]; then
+    	echo "-- ${d}"
+    fi
+done
